a08a6f2d9b5939a6e12b083f46c60c05a3b805bf
wanglong001 <406134592@qq.com>
2015-09-25 14:01:50 +0800

    Site updated: 2015-09-25 14:01:50

--- admin/user-add.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Zhong Xunke
- * Date: 2015/6/2
- * Time: 11:30
- */
--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <div class="bar">
-        <div class="content center bar text-center">
-            关于我们
-        </div>
-    </div>
-</footer>
